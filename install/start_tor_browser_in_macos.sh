48d6c868c2c8b02d4ec86505750b35bdd66d60b9
radio24 <anonym@torbox.ch>
2020-01-05 19:26:47 +0100

    Mitigation Tor over Tor scenario

--- install/start_tor_browser_in_macos.sh
+++ install/start_tor_browser_in_macos.sh
@@ -11,7 +11,7 @@
 # https://www.whonix.org/wiki/Other_Operating_Systems#Configure_Tor_Browser_Settings
 
 clear
-echo ""
+echo " "
 echo "This script will start the Tor Browser without Tor and link to your TorBox"
 echo "Without TorBox the Tor Browser will not work !!"
 echo "After closing the Tor Browser the script restores the old settings."
@@ -22,6 +22,8 @@ export TOR_SKIP_CONTROLPORTTEST=1
 export TOR_SKIP_LAUNCH=1
 FIREFOX_PROF_PATH=$(grep "^Path" ~/Library/Application\ Support/TorBrowser-Data/Browser/profiles.ini)
 DEFAULT=$(echo $FIREFOX_PROF_PATH |cut -c6-)
+echo " "
+echo " "
 echo "user_pref(\"extensions.torbutton.use_privoxy\", false);
 user_pref(\"extensions.torbutton.settings_method\", \"custom\");
 user_pref(\"extensions.torbutton.socks_host\", \"192.168.42.1\");
@@ -32,7 +34,14 @@ user_pref(\"extensions.torbutton.custom.socks_host\", \"192.168.42.1\");
 user_pref(\"extensions.torbutton.custom.socks_port\", 9050);
 user_pref(\"extensions.torlauncher.control_host\", \"192.168.42.1\");
 user_pref(\"extensions.torlauncher.control_port\", 9051);" | tee -a ~/Library/Application\ Support/TorBrowser-Data/Browser/$DEFAULT/user.js
-/Applications/Tor\ Browser.app/Contents/MacOS/firefox
+/Applications/Tor\ Browser.app/Contents/MacOS/firefox &
+clear
+echo " "
+echo "Tor Browser sucessful launched!"
+echo "DONT CLOSE THAT WINDOW, YET if you want to restore the original behavior of"
+echo "the Tor Browser with its own Tor instance. Close first the Tor Browser, then"
+read -n 1 -s -r -p "press any key to continue"
+
 rm ~/Library/Application\ Support/TorBrowser-Data/Browser/$DEFAULT/user.js
 sed -i -e "s/^user_pref(\"extensions.torbutton.use_privoxy\".*//g" ~/Library/Application\ Support/TorBrowser-Data/Browser/$DEFAULT/prefs.js
 sed -i -e "s/^user_pref(\"extensions.torbutton.settings_method\".*//g" ~/Library/Application\ Support/TorBrowser-Data/Browser/$DEFAULT/prefs.js
@@ -44,3 +53,4 @@ sed -i -e "s/^user_pref(\"extensions.torbutton.custom.socks_host\".*//g" ~/Libra
 sed -i -e "s/^user_pref(\"extensions.torbutton.custom.socks_port\".*//g" ~/Library/Application\ Support/TorBrowser-Data/Browser/$DEFAULT/prefs.js
 sed -i -e "s/^user_pref(\"extensions.torlauncher.control_host\".*//g" ~/Library/Application\ Support/TorBrowser-Data/Browser/$DEFAULT/prefs.js
 sed -i -e "s/^user_pref(\"extensions.torlauncher.control_port\".*//g" ~/Library/Application\ Support/TorBrowser-Data/Browser/$DEFAULT/prefs.js
+exit
