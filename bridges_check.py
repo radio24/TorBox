4d425aa96819b3917cd18c5162654d3bb108fceb
radio24 <anonym@torbox.ch>
2020-05-23 16:02:54 +0200

    Check bridges

--- bridges_check.py
+++ bridges_check.py
@@ -1,5 +1,40 @@
 #!/usr/bin/python3
 
+# This file is part of TorBox, an easy to use anonymizing router based on Raspberry Pi.
+# Copyright (C) 2020 Patrick Truffer
+# Contact: anonym@torbox.ch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it is useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# DESCRIPTION
+# This file checks the status of a bridge. Possible return values are:
+# 0: bridge exists and is offline
+# 1: bridge exists and is online
+# 2: bridge doesn't exist
+#
+# SYNTAX
+# ./bridges_check [-i] -f <fingerprint> [-s] [--info file_name] [-h]
+#
+# -h: print the help screen
+# --help: print the help screen
+# -f <fingerprint>: search with the fingerprint
+# -f <fingerprint> -s: search with the hashed fingerprint
+# -f <fingerprint> -i: search with the fingerprint and print extended information on stdout
+# -f <fingerprint> -s -i: search with the hashed fingerprint and print extended information on stdout
+# -f <fingerprint> --info file_name: search with the fingerprint and save the extended information into file_name
+# -f <fingerprint> -s --info file_name: search with the hashed fingerprint and save the extended information into file_name
+
 import sys
 import getopt
 import requests
@@ -9,7 +44,7 @@ from binascii import a2b_hex
 from hashlib import sha1
 
 # get the options from cmd line
-options, remainder = getopt.getopt(sys.argv[1:], 'f:ish', ['fingerprint=', 
+options, remainder = getopt.getopt(sys.argv[1:], 'f:ish', ['fingerprint=',
                                                         'info=',
                                                         'help',
                                                         'hashed-fingerprint'
@@ -29,7 +64,7 @@ for opt, arg in options:
     elif opt in ('-s', '--hashed-fingerprint'):
         hashed_fingerprint = True
     elif opt in ('-h', '--help'):
-        print("Usage:\n %s [-i] -f <fingerprint>\n\nOptions:\n -f, --fingerprint=<fingerprint>\tGet status of a tor bridge (0: online, 1: offline, 2: not exists) [REQUIRED PARAM]\n\t\t\t\t\t Fingerprint must not be hashed\n -s, --hashed-fingerprint\t\tSearch for hashed fingerprint\n -i, --info <file_name>\t\t\tSave the info from bridge and save to file in JSON format (-i prints to stdout)\n -h, --help\t\t\t\tshow this help\n" % sys.argv[0])
+        print("Usage:\n %s [-i] -f <fingerprint>\n\nOptions:\n -f, --fingerprint=<fingerprint>\tGet status of a tor bridge (0: offline, 1: online, 2: not exists) [REQUIRED PARAM]\n\t\t\t\t\t Fingerprint must not be hashed\n -s, --hashed-fingerprint\t\tSearch for hashed fingerprint\n -i, --info <file_name>\t\t\tSave the info from bridge and save to file in JSON format (-i prints to stdout)\n -h, --help\t\t\t\tshow this help\n" % sys.argv[0])
         quit()
 
 # if fingerprint not passed, we show how to use it. fingerprint is required
@@ -68,7 +103,7 @@ if len(data['bridges']):
     # Not running
     else:
         res = 0 # OFFLINE
-    
+
     if show_info:
         print("%s:{}".format(b) % (res))
     else:
