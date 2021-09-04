e333416b27017b4d04de40de32c92d5915852b91
radio24 <anonym@torbox.ch>
2021-09-04 17:00:01 +0500

    Improved: first start-up procedure

--- install/log_check_config/log_check_config.py
+++ install/log_check_config/log_check_config.py
@@ -47,14 +47,14 @@ matches = [
     {
         "file": '/var/log/tor/notices.log',
         "match": "*We tried for * seconds to connect to * using exit *",  # use * as wildcard
-        "match_count": 40,  # min matches to execute command
+        "match_count": 30,  # min matches to execute command
         "match_time": 2*60,  # (seconds) time range of match count to execute cmd
         "command": 'sudo bash /home/torbox/torbox/automat 3',
     },
     {
         "file": '/var/log/tor/notices.log',
         "match": "*Tried for * seconds to get a connection to * Giving up*",  # use * as wildcard
-        "match_count": 40,  # min matches to execute command
+        "match_count": 30,  # min matches to execute command
         "match_time": 2*60,  # (seconds) time range of match count to execute cmd
         "command": 'sudo bash /home/torbox/torbox/automat 3',
     },
