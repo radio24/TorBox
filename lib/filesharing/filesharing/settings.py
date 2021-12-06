f83e368418a14877335b7f12d9b464bcf058f672
Carlos Martínez <carlosmartinez87@gmail.com>
2021-12-06 18:02:18 -0300

    fixed call from external dir

--- lib/filesharing/filesharing/settings.py
+++ lib/filesharing/filesharing/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'filesharing.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
