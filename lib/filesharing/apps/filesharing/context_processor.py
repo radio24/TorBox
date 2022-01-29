d33ce41500f50df6601ab4d25c7f9c7c777c273e
Carlos Martínez <carlosmartinez87@gmail.com>
2022-01-29 18:52:55 -0300

    allow new lines in admin msg header

--- lib/filesharing/apps/filesharing/context_processor.py
+++ lib/filesharing/apps/filesharing/context_processor.py
@@ -4,6 +4,6 @@ def filesharing_context(request):
     obj = {
         'ALLOW_UPLOAD': settings.ALLOW_UPLOAD,
         'ALLOW_DOWNLOAD': settings.ALLOW_DOWNLOAD,
-        'MSG_HEADER': settings.MSG_HEADER
+        'MSG_HEADER': settings.MSG_HEADER.replace('\\n', '\n')
     }
     return obj
