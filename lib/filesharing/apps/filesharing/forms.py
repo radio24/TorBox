8d6a6120ea7190e26c627644f01a64e821151e1f
Carlos Martínez <carlosmartinez87@gmail.com>
2022-01-29 18:55:45 -0300

    upload subfolder + zip multi download

--- lib/filesharing/apps/filesharing/forms.py
+++ lib/filesharing/apps/filesharing/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import DownloadFileModel
 
 class UploadFileForm(forms.ModelForm):
+    subfolder = forms.BooleanField(required=False, initial=False)
+    
     def __init__(self, *args, **kwargs):
         super(UploadFileForm, self).__init__(*args, **kwargs)
         self.fields['file'].required = True
@@ -11,3 +13,9 @@ class UploadFileForm(forms.ModelForm):
         widget = {
             'file': forms.ClearableFileInput(attrs={'multiple': True})
         }
+
+class DownloadZipForm(forms.Form):
+    file_list = forms.ModelMultipleChoiceField(
+        queryset=DownloadFileModel.objects.exclude(pk=1),
+        widget=forms.CheckboxSelectMultiple,
+    )
