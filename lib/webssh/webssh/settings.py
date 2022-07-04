f3e0545511af4b21af5516ccad175545d28a79c7
radio24 <anonym@torbox.ch>
2022-07-04 21:36:31 +0200

    webssh: change to port 9000

--- lib/webssh/webssh/settings.py
+++ lib/webssh/webssh/settings.py
@@ -24,7 +24,7 @@ define('wifi', type=bool, default=True)
 define('unix_socket', default=f'/run/user/{os.getuid()}/torbox/webssh.sock',
        help='Path to unix socket')
 define('address', default='', help='Listen address')
-define('port', type=int, default=8888,  help='Listen port')
+define('port', type=int, default=9000,  help='Listen port')
 define('ssladdress', default='', help='SSL listen address')
 define('sslport', type=int, default=4433,  help='SSL listen port')
 define('certfile', default='', help='SSL certificate file')
