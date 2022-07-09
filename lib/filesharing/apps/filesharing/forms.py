aa74257dbd13eb7377c8ce156540e409e8106ad6
Carlos Martínez <carlosmartinez87@gmail.com>
2022-07-09 08:23:51 -0400

    refactor

--- lib/filesharing/apps/filesharing/forms.py
+++ lib/filesharing/apps/filesharing/forms.py
@@ -1,18 +1,19 @@
 from django import forms
 from .models import DownloadFileModel
 
+
 class UploadFileForm(forms.ModelForm):
     subfolder = forms.IntegerField(required=False, initial=False)
 
     def __init__(self, *args, **kwargs):
         super(UploadFileForm, self).__init__(*args, **kwargs)
-        self.fields['file'].required = True
+        self.fields["file"].required = True
+
     class Meta:
         model = DownloadFileModel
-        fields = ['file']
-        widget = {
-            'file': forms.ClearableFileInput(attrs={'multiple': True})
-        }
+        fields = ["file"]
+        widget = {"file": forms.ClearableFileInput(attrs={"multiple": True})}
+
 
 class DownloadZipForm(forms.Form):
     file_list = forms.ModelMultipleChoiceField(
--- lib/filesharing/apps/filesharing/views.py
+++ lib/filesharing/apps/filesharing/views.py
@@ -14,81 +14,86 @@ from wsgiref.util import FileWrapper
 from .forms import DownloadZipForm, UploadFileForm
 from .models import DownloadFileModel
 
-# Create your views here.
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
 
 def upload(request, subfolder_id=None):
     if not settings.ALLOW_UPLOAD:
-        return redirect('index')
+        return redirect("index")
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            if form.data.get('subfolder', None):
-                subfolder_id = form.data['subfolder']
+            if form.data.get("subfolder", None):
+                subfolder_id = form.data["subfolder"]
                 parent = DownloadFileModel.objects.get(pk=subfolder_id)
 
             else:
                 parent = DownloadFileModel.objects.get(pk=1)
-            files = request.FILES.getlist('file')
+            files = request.FILES.getlist("file")
             for f in files:
-                file_instance = DownloadFileModel(name=f.name,
-                                                file=f,
-                                                size=f.size,
-                                                parent=parent)
+                file_instance = DownloadFileModel(
+                    name=f.name, file=f, size=f.size, parent=parent
+                )
                 file_instance.save()
 
-            return render(request, 'upload_ok.html')
+            return render(request, "upload_ok.html")
         else:
-            return render(request, "upload.html", {'error': True})
+            return render(request, "upload.html", {"error": True})
     else:
         tpl = {}
         if subfolder_id:
             subfolder = DownloadFileModel.objects.get(pk=subfolder_id)
-            tpl['subfolder'] = subfolder
-            tpl['path'] = get_file_object_path(subfolder)
+            tpl["subfolder"] = subfolder
+            tpl["path"] = get_file_object_path(subfolder)
         return render(request, "upload.html", tpl)
+
 
 class DownloadListView(ListView):
     model = DownloadFileModel
-    template_name = 'download.html'
-    context_object_name = 'files'
-    #paginate_by = 10
+    template_name = "download.html"
+    context_object_name = "files"
+    # paginate_by = 10
 
     def dispatch(self, request, *args, **kwargs):
         if not settings.ALLOW_DOWNLOAD:
-            return redirect('index')
+            return redirect("index")
 
         return super(DownloadListView, self).dispatch(request, *args, **kwargs)
 
     def get_queryset(self):
-        return DownloadFileModel.objects.filter(parent=self.kwargs['pk'])\
-                                        .order_by('-is_dir', 'name')
+        return DownloadFileModel.objects.filter(parent=self.kwargs["pk"]).order_by(
+            "-is_dir", "name"
+        )
 
     def get_context_data(self, **kwargs):
         context = super(DownloadListView, self).get_context_data(**kwargs)
         # Get parent
-        obj = DownloadFileModel.objects.get(pk=self.kwargs['pk'])
+        obj = DownloadFileModel.objects.get(pk=self.kwargs["pk"])
         try:
             parent_pk = obj.parent.pk
         except:
             parent_pk = False
-        context['pk'] = self.kwargs['pk']
-        context['path'] = get_file_object_path(obj)
-        context['parent'] = parent_pk
+        context["pk"] = self.kwargs["pk"]
+        context["path"] = get_file_object_path(obj)
+        context["parent"] = parent_pk
         return context
 
+
 def download_zip(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = DownloadZipForm(request.POST)
         if form.is_valid():
             path_list = []
-            file_list = form.cleaned_data['file_list']
+            file_list = form.cleaned_data["file_list"]
             for file in file_list:
-                file_info = (f"{settings.MEDIA_ROOT}/{get_file_object_path(file)}",
-                            file.name,
-                            file.is_dir)
+                file_info = (
+                    f"{settings.MEDIA_ROOT}/{get_file_object_path(file)}",
+                    file.name,
+                    file.is_dir,
+                )
                 path_list.append(file_info)
             # Calculate size of download
             total_size = 0
@@ -99,40 +104,45 @@ def download_zip(request):
             # Check current disk status
             total, used, free = shutil.disk_usage("/")
 
-            # We have enought free space to go
+            # Enough free space to go
             if total_size < free:
                 # Generate zip in temp file
                 temp = tempfile.TemporaryFile()
-                with zipfile.ZipFile(temp, 'w', zipfile.ZIP_DEFLATED) as archive:
+                with zipfile.ZipFile(temp, "w", zipfile.ZIP_DEFLATED) as archive:
                     for f in path_list:
                         path = f[0]
                         filename = f[1]
                         is_dir = f[2]
                         if is_dir:
                             src_path = Path(path).expanduser().resolve(strict=True)
-                            for file in src_path.rglob('*'):
+                            for file in src_path.rglob("*"):
                                 archive.write(file, file.relative_to(src_path.parent))
                         else:
                             archive.write(path, filename)
                 # Serve zip
                 random = uuid.uuid4()
-                response = StreamingHttpResponse(FileWrapper(temp), content_type='application/zip')
-                response['Content-Disposition'] = f'attachment; filename=torbox-tfs_{random}.zip'
-                response['Content-Length'] = temp.tell()
+                response = StreamingHttpResponse(
+                    FileWrapper(temp), content_type="application/zip"
+                )
+                response[
+                    "Content-Disposition"
+                ] = f"attachment; filename=torbox-tfs_{random}.zip"
+                response["Content-Length"] = temp.tell()
                 temp.seek(0)
                 return response
             else:
-                errors = {'type': "DISK_SPACE_ERROR"}
-                return render(request, 'error.html', errors)
+                errors = {"type": "DISK_SPACE_ERROR"}
+                return render(request, "error.html", errors)
         else:
-            errors = {'type': "SELECTION_ERROR"}
-            return render(request, 'error.html', errors)
-            #raise Http404
+            errors = {"type": "SELECTION_ERROR"}
+            return render(request, "error.html", errors)
+            # raise Http404
     else:
         raise Http404
 
+
 def get_file_object_path(obj):
-     # return path from media root
+    # return path from media root
     path = []
     while obj.pk != 1:
         path.append(obj.name)
