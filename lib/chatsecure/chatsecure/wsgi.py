3812cc1fc9b7ac07f0af610417d3d5cad86db9f5
Carlos Martínez <carlosmartinez87@gmail.com>
2022-10-28 11:29:51 -0300

    add instance, unix socket, deployment

--- lib/chatsecure/chatsecure/wsgi.py
+++ lib/chatsecure/chatsecure/wsgi.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os
 import socketio
+import eventlet
+import socket
 
 from django.core.wsgi import get_wsgi_application
 from django.conf import settings
@@ -16,14 +18,20 @@ from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatsecure.settings')
 
 application = get_wsgi_application()
+app_socket = (f"unix:/var/run/tcs_{settings.INSTANCE_NAME}.sock",)
+app_socket_family = socket.AF_UNIX
 
 if settings.DEBUG:
     # dev
     from django.contrib.staticfiles.handlers import StaticFilesHandler
     application = StaticFilesHandler(get_wsgi_application())
+    app_socket = ('127.0.0.1', 8010)
+    app_socket_family = socket.AF_INET
 
 from apps.socketio_app.views import sio
 application = socketio.WSGIApp(sio, application)
-
-import eventlet
-eventlet.wsgi.server(eventlet.listen(('127.0.0.1', 8010)), application)
+# eventlet.wsgi.server(eventlet.listen(('127.0.0.1', 8010)), application)
+# eventlet.wsgi.server(
+#         sock=eventlet.listen(app_socket),
+#         site=application
+#     )
