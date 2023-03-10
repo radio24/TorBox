ca813fb592ca62c012bfa94ce095d38918559060
radio24 <anonym@torbox.ch>
2023-03-10 09:40:48 +0900

    Update: go 1.19.4 -> 1.20.2

--- install/run_install_on_debian.sh
+++ install/run_install_on_debian.sh
@@ -81,8 +81,8 @@ NOCOLOR='\033[0m'
 NAMESERVERS="1.1.1.1,1.0.0.1,8.8.8.8,8.8.4.4"
 
 # Used go version
-GO_VERSION="go1.19.4.linux-armv6l.tar.gz"
-GO_VERSION_64="go1.19.4.linux-arm64.tar.gz"
+GO_VERSION="go1.20.2.linux-armv6l.tar.gz"
+GO_VERSION_64="go1.20.2.linux-arm64.tar.gz"
 GO_DL_PATH="https://golang.org/dl/"
 
 # Release Page of the unofficial Tor repositories on GitHub
