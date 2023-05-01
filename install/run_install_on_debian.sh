5093d063027f57cf2d1ee346e4075f26f80553b0
radio24 <anonym@torbox.ch>
2023-05-01 20:09:42 +0900

    FIX: not connected interfaces have also to put down for macchanger

--- install/run_install_on_debian.sh
+++ install/run_install_on_debian.sh
@@ -652,7 +652,11 @@ if [ ! -z "$GO_VERSION_NR" ] || [ "$GO_VERSION_NR" -lt "17" ]; then
 			sleep 10
 		fi
 		re-connect
-		apt-get -y -t bullseye-backports install golang
+		if grep "^deb http://deb.debian.org/debian bullseye-backports main" /etc/apt/sources.list ; then
+			apt-get -y -t bullseye-backports install golang
+		else
+			apt-get -y install golang
+		fi
 	else
   	tar -C /usr/local -xzvf $DOWNLOAD
 		rm $DOWNLOAD
