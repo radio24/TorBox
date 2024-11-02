d9bb2eb05e33521b9c63fdfe19477945db40e940
radio24 <anonym@torbox.ch>
2024-11-02 23:17:04 +0100

    Fix: requirements check in prepare_image.sh

--- install/prepare_image.sh
+++ install/prepare_image.sh
@@ -158,13 +158,17 @@ if [ ! -f requirements.txt ]; then
 	sudo sed -i "/^pillow==.*/g" requirements.txt
 	sudo sed -i "s/^typing-extensions==/typing_extensions==/g" requirements.txt
 fi
-if [ -f requirements.failed ]; then rm requirements.failed; fi
+# NEW v.0.5.4
+# grep -v '^\s*$ filters out empty lines or lines containing only whitespace.
+# tail -n +2 will skipp the first line
+readarray -t REQUIREMENTS < <(grep -v '^\s*$' requirements.txt | tail -n +2)
 REPLY="Y"
 while [ "$REPLY" == "Y" ] || [ "$REPLY" == "y" ]; do
   unset REPLY
   readarray -t REQUIREMENTS < requirements.txt
   for REQUIREMENT in "${REQUIREMENTS[@]}"; do
-    if grep "==" <<< $REQUIREMENT ; then REQUIREMENT=$(sed s"/==.*//" <<< $REQUIREMENT); fi
+		# NEW v.0.5.4
+		if grep "==" <<< $REQUIREMENT ; then REQUIREMENT=$(sed s"/==.*//" <<< $REQUIREMENT); fi
     VERSION=$(pip3 freeze | grep -i $REQUIREMENT | sed "s/${REQUIREMENT}==//i" 2>&1)
     echo -e "${RED}${REQUIREMENT} version: ${YELLOW}$VERSION${NOCOLOR}"
     if [ -z "$VERSION" ]; then
